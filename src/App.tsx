e6fcfde9b4b8d439e22e5366f97c63f35741a23a
Jennifer <jenniferromanmuerte@gmail.com>
2025-09-12 11:39:30 +0200

    Más cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Experience from "./pages/Experience/Experience";
 import Projects from "./pages/Projects/Projects";
-import LoadingOverlay from "./components/LoadingOverlay/LoadingOverlay";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
   return (
     <Router>
-      {isLoading && <LoadingOverlay visible={isLoading} />}
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <div className="flex-grow p-4">
+        <div className="flex-grow p-0">
           <Routes>
-            <Route path="/" element={<Home onReady={() => setIsLoading(false)} />} />
+            <Route path="/" element={<Home />} />
             <Route path="/experience" element={<Experience />} />
             <Route path="/projects" element={<Projects />} />
           </Routes>
